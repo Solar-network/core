021438df580abbfbd6fbf72bd79f07d34775369e
alessiodf <35549818+alessiodf@users.noreply.github.com>
2022-03-21 15:11:19 +0000

    chore(core-kernel): fix upstream typo

--- packages/core-kernel/src/services/config/drivers/local.ts
+++ packages/core-kernel/src/services/config/drivers/local.ts
@@ -216,6 +216,6 @@ export class LocalConfigLoader implements ConfigLoader {
             }
         }
 
-        throw new FileException(`Failed to discovery any files matching [${files.join(", ")}]`);
+        throw new FileException(`Failed to discover any files matching [${files.join(", ")}]`);
     }
 }
