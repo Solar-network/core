85f10cb346093b02507aa2912142ad502bdc757d
alessiodf <35549818+alessiodf@users.noreply.github.com>
2022-05-02 19:19:50 +0000

    fix(core-api): allow searching by height again

--- packages/core-api/src/routes/blocks.ts
+++ packages/core-api/src/routes/blocks.ts
@@ -64,7 +64,7 @@ export const register = (server: Hapi.Server): void => {
         options: {
             validate: {
                 params: Joi.object({
-                    id: Joi.string().length(64).hex(),
+                    id: Joi.alternatives().try(Joi.string().length(64).hex(), Joi.number().integer().min(1)),
                 }),
                 query: Joi.object({
                     transform: Joi.bool().default(true),
