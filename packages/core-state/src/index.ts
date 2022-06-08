17216c0a84c38a9b2754f70c7cef12968dcfa13e
alessiodf <35549818+alessiodf@users.noreply.github.com>
2022-06-08 15:11:17 +0000

    refactor(core-state): remove loader from index

--- packages/core-state/src/index.ts
+++ packages/core-state/src/index.ts
@@ -3,5 +3,4 @@ export * as Wallets from "./wallets";
 export { DatabaseInteraction } from "./database-interactions";
 export { DatabaseInterceptor } from "./database-interceptor";
 export { StateBuilder } from "./state-builder";
-export { StateLoader } from "./state-loader";
 export * as Stores from "./stores";
