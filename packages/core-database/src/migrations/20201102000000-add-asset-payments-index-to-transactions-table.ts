ea80201bfc15e2ec2ee7116a87ad50af2c6ad57b
alessiodf <35549818+alessiodf@users.noreply.github.com>
2022-06-13 22:14:36 +0000

    chore(core-database): restore original migration

--- packages/core-database/src/migrations/20201102000000-add-asset-payments-index-to-transactions-table.ts
+++ packages/core-database/src/migrations/20201102000000-add-asset-payments-index-to-transactions-table.ts
@@ -3,13 +3,13 @@ import { MigrationInterface, QueryRunner } from "typeorm";
 export class AddAssetPaymentsIndexToTransactionsTable20201102000000 implements MigrationInterface {
     public async up(queryRunner: QueryRunner): Promise<any> {
         await queryRunner.query(`
-            CREATE INDEX IF NOT EXISTS transactions_asset_transfers ON transactions USING GIN ((asset->'transfers'));
+            CREATE INDEX IF NOT EXISTS transactions_asset_payments ON transactions USING GIN ((asset->'payments'));
         `);
     }
 
     public async down(queryRunner: QueryRunner): Promise<any> {
         await queryRunner.query(`
-            DROP INDEX transactions_asset_transfers;
+            DROP INDEX transactions_asset_payments;
         `);
     }
 }
