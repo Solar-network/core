3ac3f2df1d4755e509c8ec2abe7b35bde63daf27
alessiodf <35549818+alessiodf@users.noreply.github.com>
2022-06-15 11:38:42 +0000

    chore(core-database): more descriptive migration name

--- packages/core-database/src/migrations/20220608000000-rename-serialized-to-serialised-in-transactions-table.ts
+++ packages/core-database/src/migrations/20220608000000-rename-serialized-to-serialised-in-transactions-table.ts
@@ -1,6 +1,6 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
-export class RenameSerializedToSerialised20220608000000 implements MigrationInterface {
+export class RenameSerializedToSerialisedInTransactionsTable20220608000000 implements MigrationInterface {
     public async up(queryRunner: QueryRunner): Promise<any> {
         await queryRunner.query(`
             ALTER TABLE transactions RENAME serialized TO serialised;
