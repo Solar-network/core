eaa8a621338b63db1418c1babd30717ee57b2de9
alessiodf <35549818+alessiodf@users.noreply.github.com>
2022-08-02 22:23:05 +0000

    fix: prevent adding empty wallets to repository

--- packages/state/src/wallets/indexers/indexers.ts
+++ packages/state/src/wallets/indexers/indexers.ts
@@ -1,31 +1,40 @@
 import { Contracts } from "@solar-network/kernel";
 
+const isIndexable = (wallet: Contracts.State.Wallet): boolean => {
+    return (
+        !wallet.getNonce().isZero() ||
+        !wallet.getBalance().isZero() ||
+        wallet.hasMultiSignature() ||
+        Object.keys(wallet.getAttributes()).length > 1
+    );
+};
+
 export const addressesIndexer = (index: Contracts.State.WalletIndex, wallet: Contracts.State.Wallet): void => {
-    if (wallet.getAddress()) {
+    if (wallet.getAddress() && isIndexable(wallet)) {
         index.set(wallet.getAddress(), wallet);
     }
 };
 
 export const publicKeysIndexer = (index: Contracts.State.WalletIndex, wallet: Contracts.State.Wallet): void => {
-    if (wallet.getPublicKey()) {
+    if (wallet.getPublicKey() && isIndexable(wallet)) {
         index.set(wallet.getPublicKey()!, wallet);
     }
 };
 
 export const usernamesIndexer = (index: Contracts.State.WalletIndex, wallet: Contracts.State.Wallet): void => {
-    if (wallet.isDelegate()) {
+    if (wallet.isDelegate() && isIndexable(wallet)) {
         index.set(wallet.getAttribute("delegate.username"), wallet);
     }
 };
 
 export const resignationsIndexer = (index: Contracts.State.WalletIndex, wallet: Contracts.State.Wallet): void => {
-    if (wallet.isDelegate() && wallet.hasAttribute("delegate.resigned")) {
+    if (wallet.isDelegate() && wallet.hasAttribute("delegate.resigned") && isIndexable(wallet)) {
         index.set(wallet.getAttribute("delegate.username"), wallet);
     }
 };
 
 export const locksIndexer = (index: Contracts.State.WalletIndex, wallet: Contracts.State.Wallet): void => {
-    if (wallet.hasAttribute("htlc.locks")) {
+    if (wallet.hasAttribute("htlc.locks") && isIndexable(wallet)) {
         const locks: object = wallet.getAttribute("htlc.locks");
 
         for (const lockId of Object.keys(locks)) {
@@ -35,7 +44,7 @@ export const locksIndexer = (index: Contracts.State.WalletIndex, wallet: Contrac
 };
 
 export const ipfsIndexer = (index: Contracts.State.WalletIndex, wallet: Contracts.State.Wallet): void => {
-    if (wallet.hasAttribute("ipfs.hashes")) {
+    if (wallet.hasAttribute("ipfs.hashes") && isIndexable(wallet)) {
         const hashes: object = wallet.getAttribute("ipfs.hashes");
 
         for (const hash of Object.keys(hashes)) {
