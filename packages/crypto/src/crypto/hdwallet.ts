5c406b87865c61960be881f2af1c7399b35ad5fc
alessiodf <35549818+alessiodf@users.noreply.github.com>
2022-08-12 15:11:13 +0100

    refactor(crypto): use bip32 implementation compatible with node v17+ (#105)

--- packages/crypto/src/crypto/hdwallet.ts
+++ packages/crypto/src/crypto/hdwallet.ts
@@ -1,56 +1,46 @@
-import { BIP32Interface, fromPrivateKey, fromSeed } from "bip32";
+import { HDKey } from "@scure/bip32";
 import { mnemonicToSeedSync } from "bip39";
 
 import { IKeyPair } from "../interfaces";
 import { configManager } from "../managers";
 
 export class HDWallet {
-    public static readonly slip44 = 3333;
-
     /**
      * Get root node from the given mnemonic with an optional passphrase.
      */
-    public static fromMnemonic(mnemonic: string, passphrase?: string): BIP32Interface {
-        return fromSeed(mnemonicToSeedSync(mnemonic, passphrase), configManager.get("network"));
+    public static fromMnemonic(mnemonic: string, account: number, slip44?: number, passphrase?: string): HDKey {
+        if (slip44 === undefined) {
+            slip44 = configManager.get("network.slip44");
+        }
+        return HDKey.fromMasterSeed(mnemonicToSeedSync(mnemonic, passphrase)).derive(`m/44'/${slip44}'/${account}'/0`);
     }
 
     /**
-     * Get bip32 node from keys.
+     * Get key pair from the given root, optionally at the specified index.
      */
-    public static fromKeys(keys: IKeyPair, chainCode: Buffer): BIP32Interface {
-        if (!keys.compressed) {
-            throw new TypeError("BIP32 only allows compressed keys");
+    public static getKeys(root: HDKey, index?: number): IKeyPair {
+        let node: HDKey = root;
+
+        if (index !== undefined) {
+            node = root.deriveChild(index);
         }
 
-        return fromPrivateKey(Buffer.from(keys.privateKey, "hex"), chainCode, configManager.get("network"));
-    }
-
-    /**
-     * Get key pair from the given node.
-     */
-    public static getKeys(node: BIP32Interface): IKeyPair {
-        if (!node.privateKey) {
+        if (!node.privateKey || !node.publicKey) {
             throw new Error();
         }
 
         return {
-            publicKey: node.publicKey.toString("hex"),
-            privateKey: node.privateKey.toString("hex"),
+            publicKey: Buffer.from(
+                node.publicKey.buffer,
+                node.publicKey.byteOffset,
+                node.publicKey.byteLength,
+            ).toString("hex"),
+            privateKey: Buffer.from(
+                node.privateKey.buffer,
+                node.privateKey.byteOffset,
+                node.privateKey.byteLength,
+            ).toString("hex"),
             compressed: true,
         };
-    }
-
-    /**
-     * Derives a node from the coin type as specified by slip44.
-     */
-    public static deriveSlip44(root: BIP32Interface, hardened = true): BIP32Interface {
-        return root.derivePath(`m/44'/${this.slip44}${hardened ? "'" : ""}`);
-    }
-
-    /**
-     * Derives a node from the network as specified by AIP20.
-     */
-    public static deriveNetwork(root: BIP32Interface): BIP32Interface {
-        return this.deriveSlip44(root).deriveHardened(configManager.get("network.aip20") || 1);
     }
 }
