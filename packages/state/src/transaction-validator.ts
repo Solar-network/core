221a2c20c1578149dbf339af51d86b44156b5411
alessiodf <35549818+alessiodf@users.noreply.github.com>
2023-01-26 23:24:54 +0000

    refactor(state): do not re-check already checked transactions

--- packages/state/src/transaction-validator.ts
+++ packages/state/src/transaction-validator.ts
@@ -1,7 +1,6 @@
 import { Interfaces, Transactions } from "@solar-network/crypto";
 import { Container, Contracts } from "@solar-network/kernel";
 import { Handlers } from "@solar-network/transactions";
-import { strictEqual } from "assert";
 
 @Container.injectable()
 export class TransactionValidator implements Contracts.State.TransactionValidator {
@@ -10,8 +9,10 @@ export class TransactionValidator implements Contracts.State.TransactionValidato
     private readonly handlerRegistry!: Handlers.Registry;
 
     public async validate(transaction: Interfaces.ITransaction): Promise<Interfaces.ITransaction> {
-        const deserialised: Interfaces.ITransaction = Transactions.TransactionFactory.fromBytes(transaction.serialised);
-        strictEqual(transaction.id, deserialised.id);
+        const deserialised: Interfaces.ITransaction = Transactions.TransactionFactory.fromBytesUnsafe(
+            transaction.serialised,
+            transaction.data.id,
+        );
         const handler = await this.handlerRegistry.getActivatedHandlerForTransaction(deserialised);
         await handler.apply(deserialised);
         return deserialised;
