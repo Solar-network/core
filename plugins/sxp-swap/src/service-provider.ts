3ffe436729608e1baa99bbee2ff2f4f2b5e6c7b8
alessiodf <35549818+alessiodf@users.noreply.github.com>
2023-07-19 23:27:54 +0100

    refactor: deactivate sxp-swap (#140)

--- plugins/sxp-swap/src/service-provider.ts
+++ plugins/sxp-swap/src/service-provider.ts
@@ -1,16 +1,7 @@
-import { Container, Contracts, Providers } from "@solar-network/kernel";
-
-import { SXPSwap } from "./sxp-swap";
+import { Providers } from "@solar-network/kernel";
 
 export class ServiceProvider extends Providers.ServiceProvider {
     public async register(): Promise<void> {
-        if (this.config().get("enabled")) {
-            const logger = this.app.get<Contracts.Kernel.Logger>(Container.Identifiers.LogService);
-            const sxpSwap = Symbol.for("SXPSwap");
-
-            this.app.bind<SXPSwap>(sxpSwap).to(SXPSwap).inSingletonScope();
-            this.app.get<SXPSwap>(sxpSwap).register();
-            logger.info("Loaded SXP Swap Plugin");
-        }
+        // no-op; plugin will be removed entirely in 5.0.
     }
 }
